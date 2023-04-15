3b4d5658e3479f29af07032523737b2ef92a75ae
donfortune <donfortunet.df@gmail.com>
2023-04-15 11:33:18 +0100

    initial commit

---
+++ weather.py
@@ -0,0 +1,23 @@
+import requests
+
+
+API_KEY = '09e3d262baa11ce9e6ae8129322b7f3b'
+URL = 'http://api.openweathermap.org/data/2.5/weather'
+
+city = input('Enter City Name: ')
+request_url = f"{URL}?appid={API_KEY}&q={city}"
+
+response = requests.get(request_url)
+content = response.json()
+
+if response.status_code == 200:
+    print('all good to go')
+else:
+    print('theres an error')
+#print(content)
+weather = content['weather'][0]['description']
+print(weather)
+temperature = content['main']['temp']
+print(temperature)
+
+
