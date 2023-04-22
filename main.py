54951656dc885b6a36c20f74b0bdae1cf8c18683
donfortune <donfortunet.df@gmail.com>
2023-04-22 12:42:08 +0100

    added multiple url end points

--- main.py
+++ main.py
@@ -23,5 +23,19 @@ def about(station, date):
 
     }
 
+@app.route('/api/v1/<station>')
+def show_all_data(station):
+    main_files = "data-small/TG_STAID" + str(station).zfill(6) + '.txt'
+    data = pd.read_csv(main_files, skiprows=20, parse_dates=['    DATE'])
+    results = data.to_dict(orient='records')
+    return results
+
+@app.route('/api/v1/anu/<station>/<year>')
+def show_all_data_year(station, year):
+    main_files = "data-small/TG_STAID" + str(station).zfill(6) + '.txt'
+    data = pd.read_csv(main_files, skiprows=20)
+    data['    DATE'] = data['    DATE'].astype(str)
+    results = data[data['    DATE'].str.startswith(str(year))].to_dict(orient='records')
+    return results
 
 app.run(debug=True)
